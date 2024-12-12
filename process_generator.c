b1c9a06329076ca23f9fa11be0e5501c2d6ac325
YaraSenousy <102993483+YaraSenousy@users.noreply.github.com>
2024-12-12 23:07:58 +0200

    initialise clk in process generetor

--- process_generator.c
+++ process_generator.c
@@ -9,11 +9,13 @@ int main(int argc, char *argv[])
     // 1. Read the input files.
     // 2. Read the chosen scheduling algorithm and its parameters, if there are any from the argument list.
     // 3. Initiate and create the scheduler and clock processes.
+    int clk_id = fork();
+    if (clk_id == 0){
+        execl("./clk.out", "clk.out", NULL);
+    }
     // 4. Use this function after creating the clock process to initialize clock.
     initClk();
     // To get time use this function. 
-    int x = getClk();
-    printf("Current Time is %d\n", x);
     // TODO Generation Main Loop
     // 5. Create a data structure for processes and provide it with its parameters.
     // 6. Send the information to the scheduler at the appropriate time.
