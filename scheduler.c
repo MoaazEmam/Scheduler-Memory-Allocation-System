08d087763483aca9122c4e9c904759e35deae725
YaraSenousy <102993483+YaraSenousy@users.noreply.github.com>
2024-12-14 06:19:31 +0200

    set message queue in schedular

--- scheduler.c
+++ scheduler.c
@@ -3,7 +3,16 @@
 int main(int argc, char *argv[])
 {
     initClk();
-
+    // set message queue between process generator and schedular
+    key_t msg_id;
+    int send_val;
+    msg_id = ftok("msgfile", 65);
+    msgq_id = msgget(msg_id, 0666 | IPC_CREAT);
+    if (msgq_id == -1)
+    {
+        perror("Error in create");
+        exit(-1);
+    }
     //TODO: implement the scheduler.
     //TODO: upon termination release the clock resources.
     
